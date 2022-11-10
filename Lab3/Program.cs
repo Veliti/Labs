024aee4fc8820fa61c68fd51d8085a74c4784462
Veliti <Danilaass@gmail.com>
2022-11-10 23:09:58 +0500

    new file:   .gitignore 	new file:   Lab3/.vscode/launch.json 	new file:   Lab3/.vscode/tasks.json 	new file:   Lab3/Lab3.csproj 	new file:   Lab3/MatrixLab3.cs 	new file:   Lab3/Program.cs 	new file:   Lab3/bin/Debug/net6.0/Lab3 	new file:   Lab3/bin/Debug/net6.0/Lab3.deps.json 	new file:   Lab3/bin/Debug/net6.0/Lab3.dll 	new file:   Lab3/bin/Debug/net6.0/Lab3.pdb 	new file:   Lab3/bin/Debug/net6.0/Lab3.runtimeconfig.json 	new file:   Lab3/bin/Debug/net6.0/ref/Lab3.dll 	new file:   Lab3/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   Lab3/obj/Debug/net6.0/Lab3.AssemblyInfo.cs 	new file:   Lab3/obj/Debug/net6.0/Lab3.AssemblyInfoInputs.cache 	new file:   Lab3/obj/Debug/net6.0/Lab3.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Lab3/obj/Debug/net6.0/Lab3.GlobalUsings.g.cs 	new file:   Lab3/obj/Debug/net6.0/Lab3.assets.cache 	new file:   Lab3/obj/Debug/net6.0/Lab3.csproj.AssemblyReference.cache 	new file:   Lab3/obj/Debug/net6.0/Lab3.csproj.CoreCompileInputs.cache 	new file:   Lab3/obj/Debug/net6.0/Lab3.csproj.FileListAbsolute.txt 	new file:   Lab3/obj/Debug/net6.0/Lab3.dll 	new file:   Lab3/obj/Debug/net6.0/Lab3.genruntimeconfig.cache 	new file:   Lab3/obj/Debug/net6.0/Lab3.pdb 	new file:   Lab3/obj/Debug/net6.0/apphost 	new file:   Lab3/obj/Debug/net6.0/ref/Lab3.dll 	new file:   Lab3/obj/Lab3.csproj.nuget.dgspec.json 	new file:   Lab3/obj/Lab3.csproj.nuget.g.props 	new file:   Lab3/obj/Lab3.csproj.nuget.g.targets 	new file:   Lab3/obj/project.assets.json 	new file:   Lab3/obj/project.nuget.cache 	new file:   MatrixLab/MatrixBuilder.cs 	modified:   MatrixLab/obj/Debug/net6.0/MatrixLab.GeneratedMSBuildEditorConfig.editorconfig 	modified:   MatrixLab/obj/Debug/net6.0/MatrixLab.assets.cache

---
+++ Lab3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Task 21
+
+// var matrix1 = new MatrixLab3();
+// System.Console.WriteLine(matrix1);
+
+// var upRangeFrom = -65;
+// var upRangeTo = 120;
+// var lowRangeFrom = -3.5;
+// var lowRangeTo = 10.75;
+// var matrix2 = new MatrixLab3(4, upRangeFrom, upRangeTo, lowRangeFrom, lowRangeTo);
+// System.Console.WriteLine(matrix2);
+
+// var matrix3 = new MatrixLab3(4);
+// System.Console.WriteLine(matrix3);
+
+//Task 22
+
+// var array = new double[,]{{3, 5, 4, 2}, {7, 1, -8, 33}, {-13, 0, 6, -2}, {3, 15, 17, 4.5}};
+// var matrix4 = new MatrixLab3(array);
+// System.Console.WriteLine(matrix4);
+
+// matrix4.DiagonalSort();
+// System.Console.WriteLine(matrix4);
+
+//Task 23
+
+var A = new MatrixLab3(new double[,]{{1, 2 ,3}, {3, 2, 1}, {4, 5, 6}});
+var B = new MatrixLab3(new double[,]{{2, 6, 1}, {5, 2, 7}, {7, 1, 0}});
+var C = new MatrixLab3(new double[,]{{4, 11, 1}, {5, 2, 7}, {7, 1, 31}});
+
+System.Console.WriteLine(2 * A - B.Transpose());
